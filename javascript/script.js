c7e9c799271fa65d82b0d127184818cc3bbfaf71
Athenkosi Vinqi <nervadevelop@gmail.com>
2022-05-18 18:35:37 +0200

    date

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,12 @@
 var content = document.getElementsByTagName("body")[0];
 var darkMode = document.getElementById("dark-change");
+const dt = new Date();
 
 darkMode.addEventListener("click", function () {
   darkMode.classList.toggle("active");
   content.classList.toggle("night");
 });
+
+document.getElementById("dt").innerHTML = dt.getFullYear() ;
 
 
