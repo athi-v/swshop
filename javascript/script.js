5b02b5ffea5dd02bc3bc8e55e1aa3b95b2ff4ba8
Athenkosi Vinqi <nervadevelop@gmail.com>
2022-05-19 10:15:23 +0200

    more changes

--- javascript/script.js
+++ javascript/script.js
@@ -9,4 +9,20 @@ darkMode.addEventListener("click", function () {
 
 document.getElementById("dt").innerHTML = dt.getFullYear() ;
 
+var buts = document.getElementById('butID')
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    buts.style.display = "block";
+  } else {
+    buts.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
